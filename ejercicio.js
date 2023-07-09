21c9291bcffe78193f81f59e96d6dd97b92ebb50
alemendieta017 <amendieta017@gmail.com>
2023-07-08 21:52:19 -0400

    Ejercicio

---
+++ ejercicio.js
@@ -0,0 +1,9 @@
+let x = 2
+let y = 3
+
+x = x + y // x ahora es 5
+y = x - y // y ahora es 2
+x = x - y // x ahora es 3
+
+console.log('x =', x) // Resultado: x = 3
+console.log('y =', y) // Resultado: y = 2
